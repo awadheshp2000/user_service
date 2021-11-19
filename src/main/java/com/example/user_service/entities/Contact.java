0c0c127db5ef4c8d0d9842244be416f06b23e2b2
Awadhesh <awadheshp2000@gmail.com>
2021-11-19 20:43:48 +0000

    Initial commit

---
+++ src/main/java/com/example/user_service/entities/Contact.java
@@ -0,0 +1,6 @@
+package com.example.user_service.entities;
+
+public class Contact {
+
+
+}
---
+++ src/main/java/com/example/user_service/entities/User.java
@@ -0,0 +1,16 @@
+package com.example.user_service.entities;
+
+import java.util.ArrayList;
+
+public class User {
+
+    private Long userid;
+    private String name;
+    private String phone;
+
+
+    List <Contact> contact = new ArrayList<>();
+
+    
+            
+}
